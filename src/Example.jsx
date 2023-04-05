b2db21f5b4b56a0794d8dafc781a23baf601e792
Shibi Suriya <shibisuriya.work@gmail.com>
2023-04-05 22:37:14 +0530

    feat: sync data with states

--- src/Example.jsx
+++ src/Example.jsx
@@ -12,19 +12,32 @@ export default function Example() {
     { data: "4321", id: "1", x: 1, y: 1, w: 2, h: 2 },
     { data: "1234", id: "2", x: 1, y: 1, w: 2, h: 2 },
   ]);
+
   useEffect(() => {
-    // perform initialization or setup here
-    console.log("Component mounted!");
-    // return a cleanup function if necessary
     return () => {
       // perform cleanup here if necessary
       console.log("Component unmounted!");
     };
   }, []); // empty dependency array to ensure it only runs once
+  const widgetStyles = {
+    border: "1px solid red",
+    margin: "10px",
+  };
 
   return (
     <div>
-      <GridstackContainer>
+      {layout.map((widget, index) => {
+        return (
+          <div style={widgetStyles} key={index}>
+            <div>id: {widget.id}</div>
+            <div>x: {widget.x}</div>
+            <div>y: {widget.y}</div>
+            <div>w: {widget.w}</div>
+            <div>h: {widget.h}</div>
+          </div>
+        );
+      })}
+      <GridstackContainer layout={{ layout, setLayout }}>
         {layout.map((item) => {
           return (
             <GridstackItem
--- src/gridstack/GridstackContainer.jsx
+++ src/gridstack/GridstackContainer.jsx
@@ -2,11 +2,34 @@ import React, { useEffect } from "react";
 import "gridstack/dist/gridstack.min.css";
 import { GridStack } from "gridstack";
 export default function GridstackLayout(props) {
+  console.log("layout -> ", props.layout);
+  const init = () => {
+    const grid = GridStack.init();
+    grid.on("added change", (event, items) => {
+      for (const item in items) {
+        const { w, y, x, id } = item;
+        props.layout.setLayout((oldLayout) => {
+          return oldLayout.map((widget) => {
+            if (widget.id == id) {
+              Object.assign(widget, {
+                w,
+                y,
+                x,
+                y,
+              });
+            }
+            console.log(widget);
+            return widget;
+          });
+        });
+      }
+    });
+  };
   useEffect(() => {
     // perform initialization or setup here
     console.log("Component mounted!");
 
-    const grid = GridStack.init();
+    init();
 
     // return a cleanup function if necessary
     return () => {
