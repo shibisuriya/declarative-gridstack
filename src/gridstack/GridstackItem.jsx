0f9c44566ae1c79a7a73a615bc0c011c8745f9ad
Shibi Suriya <shibisuriya.work@gmail.com>
2023-06-03 23:37:59 +0530

    fix: reviewed and refactored, stable.

--- src/gridstack/GridstackItem.jsx
+++ src/gridstack/GridstackItem.jsx
@@ -1,62 +1,56 @@
-import React, {
-  Fragment,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import cloneDeep from "lodash/cloneDeep";
 import { MasterGridContext, SubgridContext } from "./contexts";
 
 export default function GridstackItem(props) {
-  const [itemMounted, setItemMounted] = useState(false);
+  const [areChildrenMounted, setAreChildrenMounted] = useState(false);
+
   const gsItemElement = useRef();
-  const masterGrid = useContext(MasterGridContext).current;
+  const masterGrid = useContext(MasterGridContext);
   const subgrid = useContext(SubgridContext);
-  console.log("MasterGridContext -> ", masterGrid);
-  useEffect(() => {
-    console.log("item useEffect invoked! ", Date.now());
-    if (!itemMounted) {
-      setItemMounted(true);
+
+  const getGrid = () => {
+    if (subgrid) {
+      return subgrid;
+    } else if (masterGrid) {
+      return masterGrid;
     } else {
-      if (subgrid) {
-        subgrid.makeWidget(gsItemElement.current);
-      } else if (masterGrid) {
-        masterGrid.makeWidget(gsItemElement.current);
-      } else {
-        throw new Error("Grid container not found!");
-      }
+      throw new Error("Grid container (main or subgrid) not found!");
+    }
+  };
+  useEffect(() => {
+    if (!areChildrenMounted) {
+      getGrid().makeWidget(gsItemElement.current);
+      setAreChildrenMounted(true);
+    } else {
+      throw new Error(
+        `Fatal error: Must not initialize Gridstack item with id ${props.id} multiple times.`
+      );
     }
     return () => {};
     // eslint-disable-next-line
-  }, [itemMounted]);
+  }, []);
 
-  // Copy the props which should not be mutated by the user, when the user changes a prop after the gs-item has
-  // been initialized it should not effect the gs-item. Ones the gs-item has been initialized the user shouldn't
-  // change the following, x, y, w, h and id.
-  const { x, y, w, h, id } = props;
-  const attr = cloneDeep({ x, y, w, h, id });
+  // To ensure that the user's changes to certain props do not affect the initialized gs-item, it is important to make a copy of these props. The props that should not be mutated by the user after the gs-item has been initialized are x, y, w, h, and id. Once the gs-item has been initialized, the user should refrain from modifying these specific props.
+  const { x, y, w, h, id, children, noScroll } = props;
+  const attr = cloneDeep({ x, y, w, h, id, noScroll });
   return (
-    <Fragment>
-      {itemMounted && (
-        <div
-          ref={gsItemElement}
-          className="grid-stack-item"
-          gs-x={attr.x}
-          gs-y={attr.y}
-          gs-w={attr.w}
-          gs-h={attr.h}
-          gs-id={attr.id}
-          gs-no-resize={attr.noResize}
-        >
-          <div
-            className="grid-stack-item-content"
-            style={{ overflowY: props.noScroll ? "hidden" : "auto" }}
-          >
-            {props.children}
-          </div>
-        </div>
-      )}
-    </Fragment>
+    <div
+      ref={gsItemElement}
+      className="grid-stack-item"
+      gs-x={attr.x}
+      gs-y={attr.y}
+      gs-w={attr.w}
+      gs-h={attr.h}
+      gs-id={attr.id}
+      gs-no-resize={attr.noResize}
+    >
+      <div
+        className="grid-stack-item-content"
+        style={{ overflowY: attr.noScroll ? "hidden" : "auto" }}
+      >
+        {areChildrenMounted && children}
+      </div>
+    </div>
   );
 }
