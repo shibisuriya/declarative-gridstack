9d9fc63875bb25a0c345db6b5d8ea7864e9aa19f
Shibi Suriya <shibisuriya.work@gmail.com>
2023-06-09 16:45:07 +0530

    feat: implemented dnd

--- src/gridstack/GridstackSubgrid.jsx
+++ src/gridstack/GridstackSubgrid.jsx
@@ -10,13 +10,16 @@ import {
   SubgridContext,
   UpdateLayoutContext,
   RemoveItemFromModelContext,
+  AddItemToModelContext,
 } from "./contexts";
 import getGridOptions from "./utils/getGridOptions";
+import cloneDeep from "lodash/cloneDeep";
 
 const GridstackSubgrid = React.forwardRef((props, ref) => {
   const [areChildrenMounted, setAreChildrenMounted] = useState(false);
 
   const removeItemFromModel = useContext(RemoveItemFromModelContext);
+  const addItemToModel = useContext(AddItemToModelContext);
 
   const getItemElementUsingId = (id) => {
     return subgridRef.current.querySelector(`.grid-stack-item[gs-id="${id}"]`);
@@ -48,26 +51,82 @@ const GridstackSubgrid = React.forwardRef((props, ref) => {
 
   const attachEventListeners = () => {
     subgrid.current.on("added change", (event, items) => {
-      updateLayout(items);
+      for (let item of items) {
+        if (!("id" in item)) {
+          // The item has been dragged and dropped from outside!
+          const { dnd, gridId } = props;
+          const { dndItems } = dnd;
+          if ("uidGenerator" in dnd) {
+            const { x, y, w, h, el } = item;
+            const dndItemId = el.getAttribute("gs-dnd-item-id");
+            if (dndItemId in dndItems) {
+              const dndItem = cloneDeep(dndItems[dndItemId]);
+              Object.assign(dndItem, {
+                x,
+                y,
+                w,
+                h,
+                id: String(dnd.uidGenerator()),
+              });
+              addItemToModel(dndItem, gridId); // Push item to the layout.
+            }
+            subgrid.current.removeWidget(item.el, true);
+          } else {
+            throw new Error(
+              "Fatal error: Please supply a UID generator to the grid to support drag and drop of items which doesn't belong to any gridstack grid."
+            );
+          }
+        } else if (false) {
+          // The item should not be the part of this grid.
+        } else {
+          // Item already exists in the layout model.
+          updateLayout(item);
+        }
+      }
     });
 
     subgrid.current.on("removed", (event, items) => {
+      // Don't update the model if the grid is destoryed.
       if (!isGridDestroyed.current) {
         for (let item of items) {
-          removeItemFromModel(item.id);
+          // Dnd items dont' have id, they have _id!
+          if ("id" in item) {
+            removeItemFromModel(item.id);
+          }
         }
       }
     });
   };
   useEffect(() => {
     if (!areChildrenMounted) {
-      const { accept } = props;
+      const { accept = [], dnd } = props;
+
+      const getDndOptions = () => {
+        const { dnd } = props;
+        const { options, shredder } = dnd ?? {};
+        if (dnd?.class) {
+          const gridstackDragAndDropOptions = {
+            dragIn: dnd.class,
+          };
+          if (options) {
+            gridstackDragAndDropOptions["dragInOptions"] = options;
+          }
+          if (shredder) {
+            gridstackDragAndDropOptions["removable"] = shredder;
+          }
+          return gridstackDragAndDropOptions;
+        } else {
+          return {};
+        }
+      };
+
       subgrid.current = GridStack.addGrid(subgridRef.current, {
         ...subgridOptions,
+        ...getDndOptions(),
         acceptWidgets: (el) => {
           const classList = new Set(el.classList);
           for (let i = 0; i < accept.length; i++) {
-            if (classList.has(accept[i])) {
+            if (classList.has(accept[i]) || classList.has(dnd?.class ?? "")) {
               return true;
             }
           }
