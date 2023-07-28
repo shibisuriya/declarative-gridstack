3d6d8ff4fee11663d97e24f70a3e14d56fc131cb
Shibi Suriya <shibi@kissflow.com>
2023-07-28 19:38:29 +0530

    chore: added config in react project for dev purpose

---
+++ packages/react/webpack.dev.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js",
+  },
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+  devtool: "eval-source-map",
+};
