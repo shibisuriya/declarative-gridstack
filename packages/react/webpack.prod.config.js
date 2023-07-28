74b766e8b55f70012bc25630213820761b4a708b
Shibi Suriya <shibi@kissflow.com>
2023-07-28 19:33:10 +0530

    feat: updated prod webpack config for bundling to production

---
+++ packages/react/webpack.prod.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js",
+    library: "@declarative-gridstack/react",
+    libraryTarget: "umd",
+  },
+  entry: "./src/gridstack/index.js",
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"], // Gridstack core needs to load css :)
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+  devtool: false,
+};
