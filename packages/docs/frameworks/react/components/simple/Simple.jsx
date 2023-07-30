b86e3fe816cb1633f773b9bfeb07b6527c92831a
Shibi Suriya <shibi@kissflow.com>
2023-07-30 20:28:44 +0530

    fix: unable to build vitepress

--- packages/docs/frameworks/react/components/simple/Simple.jsx
+++ packages/docs/frameworks/react/components/simple/Simple.jsx
@@ -1,62 +1,7 @@
-import React, { useState } from "react";
-import {
-  GridstackContainer,
-  GridstackItem,
-} from "@declarative-gridstack/react";
-import { JsonView, darkStyles } from "react-json-view-lite";
-import "react-json-view-lite/dist/index.css";
-import { Widget } from "../widgets/Widget";
-
-import "./styles.css";
+import React from "react";
 
 function Simple() {
-  const [layout, setLayout] = useState([
-    {
-      id: "1",
-      x: 0,
-      y: 0,
-      w: 12,
-      h: 2,
-      data: {
-        type: "calendar",
-        title: "A calendar widget",
-        data: "10/10/1990",
-      },
-    },
-  ]);
-  return (
-    <div>
-      <div className="title">
-        <div>
-          <h3>Simple</h3>
-          <p>Move and resize the grid item</p>
-        </div>
-      </div>
-      <div className="row">
-        <div className="flex-1">
-          <GridstackContainer items={layout} setLayout={setLayout}>
-            {layout.map((widget) => {
-              return (
-                <GridstackItem
-                  key={widget.id}
-                  id={widget.id}
-                  x={widget.x}
-                  y={widget.y}
-                  w={widget.w}
-                  h={widget.h}
-                >
-                  <Widget data={widget.data} />
-                </GridstackItem>
-              );
-            })}
-          </GridstackContainer>
-        </div>
-        <div className="flex-1">
-          <JsonView data={layout} style={darkStyles} class="json-viewer" />
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Simple</div>;
 }
 
 export default Simple;
