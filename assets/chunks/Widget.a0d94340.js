fd761e6de7e97d208776d629042f8f8e337c9928
shibisuriya <shibisuriya@users.noreply.github.com>
2023-08-04 12:33:54 +0000

    deploy: 4c188b26ac1c369a36418d5d2bf940f91b5236ea

---
+++ assets/chunks/Widget.a0d94340.js
@@ -0,0 +1,1 @@
+import{R as e,s as l}from"./styles.dc4cf033.js";const r=a=>{const{data:t}=a;return e.createElement("div",{className:l.widget},e.createElement("div",null,e.createElement("h1",null,t.title),e.createElement("div",null,t.data)))};export{r as W};
